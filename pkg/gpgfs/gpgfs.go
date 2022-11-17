934750eda652b250e3137804897bcf409f4d009a
Jeff Aigner <jaigner@avaya.com>
2022-11-16 17:59:47 -0800

    saving files now working

--- pkg/gpgfs/gpgfs.go
+++ pkg/gpgfs/gpgfs.go
@@ -2,6 +2,7 @@ package gpgfs
 
 import (
 	"context"
+	"fmt"
 	"github.com/ProtonMail/gopenpgp/v2/helper"
 	"github.com/hanwen/go-fuse/v2/fs"
 	"github.com/hanwen/go-fuse/v2/fuse"
@@ -12,6 +13,10 @@ import (
 )
 
 var _ = (fs.NodeOnAdder)((*GPGRoot)(nil))
+var _ = (fs.NodeFsyncer)((*GPGFile)(nil))
+var _ = (fs.NodeWriter)((*GPGFile)(nil))
+var _ = (fs.NodeCreater)((*GPGRoot)(nil))
+var _ = (fs.NodeUnlinker)((*GPGFile)(nil))
 
 type GPGRoot struct {
 	fs.Inode
@@ -22,7 +27,33 @@ type GPGRoot struct {
 	passPhrase []byte
 }
 
+func (r *GPGRoot) Unlink(ctx context.Context, name string) syscall.Errno {
+	fmt.Println("UNLINK CALLED:", name)
+	err := os.Remove("vault/" + name)
+	return fs.ToErrno(err)
+}
+
+func (r *GPGRoot) Create(ctx context.Context, name string, flags uint32, mode uint32, out *fuse.EntryOut) (node *fs.Inode, fh fs.FileHandle, fuseFlags uint32, errno syscall.Errno) {
+	errno = fuse.F_OK
+	fmt.Println("Create:", name)
+	fp, err := os.OpenFile(r.root+"/"+name, os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	node = r.NewInode(ctx, &GPGFile{file: r.root + "/" + name, root: r}, fs.StableAttr{})
+	r.AddChild(name, node, true)
+	fh = fp
+	return
+}
+
+func (r *GPGRoot) Fsync(ctx context.Context, f fs.FileHandle, flags uint32) syscall.Errno {
+	fmt.Println("GPGRoot.Fsync was called", f)
+	return syscall.F_OK
+}
+
 func (r *GPGRoot) OnAdd(ctx context.Context) {
+	fmt.Println("GPGRoot.OnAdd was called")
 	c, err := os.ReadDir(r.root)
 	if err != nil {
 		panic(err)
@@ -33,8 +64,9 @@ func (r *GPGRoot) OnAdd(ctx context.Context) {
 		if f.IsDir() {
 			continue
 		}
-		ch := p.NewPersistentInode(ctx, &gpgFile{root: r, file: r.root + "/" + f.Name()}, fs.StableAttr{})
-		p.AddChild(f.Name(), ch, true)
+		gpgfile := &GPGFile{root: r, file: r.root + "/" + f.Name()}
+		ch := p.NewPersistentInode(ctx, gpgfile, fs.StableAttr{})
+		r.Inode.AddChild(f.Name(), ch, true)
 	}
 }
 
@@ -50,7 +82,7 @@ func NewGPGTree(name, pubKey, privKey, passPhrase string) (fs.InodeEmbedder, err
 	return &GPGRoot{root: name, pubKey: pubKeyBytes, privKey: privKeyBytes, passPhrase: []byte(passPhrase)}, nil
 }
 
-type gpgFile struct {
+type GPGFile struct {
 	fs.Inode
 	file string
 	mu   sync.Mutex
@@ -58,15 +90,62 @@ type gpgFile struct {
 	root *GPGRoot
 }
 
+func (file *GPGFile) Unlink(ctx context.Context, name string) syscall.Errno {
+	fmt.Println("UNLINK CALLED:", name)
+	return fuse.F_OK
+}
+
+func (file *GPGFile) Write(ctx context.Context, f fs.FileHandle, data []byte, off int64) (written uint32, errno syscall.Errno) {
+	fmt.Println("GPGFile.Write was called")
+	encrypted, err := helper.EncryptMessageArmored(string(file.root.pubKey), string(data))
+	if err != nil {
+		fmt.Println("error writing file:", err)
+		return 0, fs.ToErrno(err)
+	}
+	err = os.WriteFile(file.file, []byte(encrypted), 0644)
+	if err != nil {
+		fmt.Println("error writing file:", err)
+		return 0, fs.ToErrno(err)
+	}
+	return uint32(len(data)), syscall.F_OK
+}
+
+func (file *GPGFile) Fsync(ctx context.Context, f fs.FileHandle, flags uint32) syscall.Errno {
+	fmt.Println("GPGRoot.Fsync was called", f)
+	return fuse.F_OK
+}
+
 // Getattr sets the minimum, which is the size. A more full-featured
 // FS would also set timestamps and permissions.
-func (file *gpgFile) Getattr(ctx context.Context, f fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
+func (file *GPGFile) Getattr(ctx context.Context, f fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
+	fmt.Println("GPGFile.Getattr was called")
+	fileStat, err := os.Stat(file.file)
+	if err != nil {
+		out.Mode = 0644
+		out.Nlink = 1
+		out.Mtime = uint64(time.Now().Unix())
+		out.Atime = out.Mtime
+		out.Ctime = out.Mtime
+		out.Owner = struct {
+			Uid uint32
+			Gid uint32
+		}{Uid: 1000, Gid: 1000}
+		out.Size = 0
+		const bs = 512
+		out.Blksize = bs
+		out.Blocks = (out.Size + bs - 1) / bs
+		return 0
+	}
 	out.Mode = 0644
 	out.Nlink = 1
-	out.Mtime = uint64(time.Now().Unix())
+	out.Mtime = uint64(fileStat.ModTime().Unix())
 	out.Atime = out.Mtime
 	out.Ctime = out.Mtime
-	out.Size = 20000
+	out.Owner = struct {
+		Uid uint32
+		Gid uint32
+	}{Uid: 1000, Gid: 1000}
+	out.Size = file.GetSize()
 	const bs = 512
 	out.Blksize = bs
 	out.Blocks = (out.Size + bs - 1) / bs
@@ -74,7 +153,8 @@ func (file *gpgFile) Getattr(ctx context.Context, f fs.FileHandle, out *fuse.Att
 }
 
 // Open lazily unpacks zip data
-func (file *gpgFile) Open(ctx context.Context, flags uint32) (fs.FileHandle, uint32, syscall.Errno) {
+func (file *GPGFile) Open(ctx context.Context, flags uint32) (fs.FileHandle, uint32, syscall.Errno) {
+	fmt.Println("GPGFile.Open was called:", file.file)
 	file.mu.Lock()
 	defer file.mu.Unlock()
 	if file.data == nil {
@@ -94,8 +174,27 @@ func (file *gpgFile) Open(ctx context.Context, flags uint32) (fs.FileHandle, uin
 	return nil, fuse.FOPEN_KEEP_CACHE, 0
 }
 
+func (file *GPGFile) GetSize() uint64 {
+	if file.data != nil {
+		return uint64(len(file.data))
+	}
+	unencrypted, err := os.ReadFile(file.file)
+	if err != nil {
+		fmt.Println("unable to get size:", err, file.file)
+		return 0
+	}
+	decrypted, err := helper.DecryptMessageArmored(string(file.root.privKey), file.root.passPhrase, string(unencrypted))
+	if err != nil {
+		fmt.Println("unable to get size:", err, file.file)
+		return 0
+	}
+	file.data = []byte(decrypted)
+	return uint64(len([]byte(decrypted)))
+}
+
 // Read simply returns the data that was already unpacked in the Open call
-func (file *gpgFile) Read(ctx context.Context, f fs.FileHandle, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
+func (file *GPGFile) Read(ctx context.Context, f fs.FileHandle, dest []byte, off int64) (fuse.ReadResult, syscall.Errno) {
+	fmt.Println("GPGFile.Read was called")
 	end := int(off) + len(dest)
 	if end > len(file.data) {
 		end = len(file.data)
